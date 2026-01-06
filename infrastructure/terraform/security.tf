b9587a432d4ada17ec7d5f63da1dc03939a9be5d
sgmselli <sellings.matthew@gmail.com>
2026-01-06 09:18:19 +0000

    Set up azure key vault and upgrade azure version

--- infrastructure/terraform/security.tf
+++ infrastructure/terraform/security.tf
@@ -9,14 +9,14 @@ resource "azurerm_network_security_group" "app" {
 }
 
 resource "azurerm_network_security_rule" "ssh_app" {
-  name                        = "Allow-SSH-Admin"
+  name                        = "Allow-SSH"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "86.14.170.85"
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.main.name
   network_security_group_name = azurerm_network_security_group.app.name
